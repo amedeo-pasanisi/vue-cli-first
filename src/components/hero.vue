d74a5ba0ec38ac5aca9527cee9cfbb11592e219c
amedeo <amedeopasanisi97@gmail.com>
2021-09-18 21:33:43 +0200

    fine esercizio

---
+++ src/components/hero.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <img src="../assets/jumbo.jpg" alt="">
+        <img src="../assets/tron.jpg" alt="">
+    </div>
+</template>
+
+<script>
+export default {
+  name: "hero"
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+</style>
